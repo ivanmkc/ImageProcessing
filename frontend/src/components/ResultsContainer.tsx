775c8407a5e4b7430ce3d05831634eda3baceba1
ivanmkc@google.com <ivanmkc@google.com>
2023-04-03 21:16:44 -0400

    Switch ResultsContainer to Tailwind

--- frontend/src/components/ResultsContainer.tsx
+++ frontend/src/components/ResultsContainer.tsx
@@ -1,21 +1,5 @@
-/**
- * Copyright 2023 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import React, { useState } from "react";
-import { Box, Paper, Tabs, Tab, Typography, Grid } from "@mui/material";
+import clsx from "clsx";
 import { ImageAnnotationResult } from "queries";
 import ImageWithBoundingBoxes from "components/ImageWithBoundingBoxes";
 import LabelDetectionResultView from "components/LabelDetectionResultView";
@@ -24,18 +8,12 @@ import SafeSearchResultView from "components/SafeSearchResultView";
 import ImagePropertiesResultView from "components/ImagePropertiesResultView";
 import FaceAnnotationsResultView from "components/FaceAnnotationsResultView";
 
-interface TabPanelProps {
-  children?: React.ReactNode;
-  index: number;
-  value: number;
-}
-
-const TabPanel = ({ children, value, index }: TabPanelProps) => {
-  return (
-    <div hidden={value !== index}>
-      {value === index && <div>{children}</div>}
-    </div>
-  );
+const tabLabels: { [key: string]: keyof ImageAnnotationResult } = {
+  Objects: "localizedObjectAnnotations",
+  Labels: "labelAnnotations",
+  Properties: "imagePropertiesAnnotation",
+  "Safe Search": "safeSearchAnnotation",
+  Faces: "faceAnnotations",
 };
 
 const ResultContainer = ({
@@ -48,122 +26,73 @@ const ResultContainer = ({
   const [selectedTab, setSelectedTab] = useState<number>(0);
   const [selectedIndex, setSelectedIndex] = useState<number>();
 
-  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+  const handleChange = (newValue: number) => {
     setSelectedTab(newValue);
   };
 
   return (
-    <Paper>
-      <Grid container spacing={4} padding={4} width="100%">
-        <Grid item xs={12} md={6}>
+    <div className="bg-white rounded-lg">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 w-full">
+        <div>
           <ImageWithBoundingBoxes
             objectAnnotations={
-              selectedTab == 0 ? result.localizedObjectAnnotations : undefined
+              selectedTab === 0 ? result.localizedObjectAnnotations : undefined
             }
             faceAnnotations={
-              selectedTab == 4 ? result.faceAnnotations : undefined
+              selectedTab === 4 ? result.faceAnnotations : undefined
             }
             imageUrl={imageUrl}
             selectedIndex={selectedIndex}
           />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <Paper sx={{ height: "100%" }}>
-            <Tabs
-              value={selectedTab}
-              onChange={handleChange}
-              variant="fullWidth"
-            >
-              <Tab
-                label="Objects"
-                disabled={result.localizedObjectAnnotations == null}
-                sx={{
-                  "&:focus": {
-                    outline: "none",
-                  },
-                }}
+        </div>
+        <div className="flex flex-col h-full">
+          <div className="tabs">
+            {Object.keys(tabLabels).map((label, index) => (
+              <button
+                key={index}
+                className={clsx(
+                  "tab tab-bordered",
+                  selectedTab === index && "tab-active",
+                  result[tabLabels[label]] == null
+                    ? "opacity-50 cursor-not-allowed"
+                    : "cursor-pointer"
+                )}
+                disabled={result[tabLabels[label]] == null}
+                onClick={() => handleChange(index)}
+              >
+                {label}
+              </button>
+            ))}
+          </div>
+          <div className="p-2 flex-grow">
+            {selectedTab === 0 && result.localizedObjectAnnotations != null && (
+              <ObjectDetectionResultView
+                annotations={result.localizedObjectAnnotations}
+                showTopResult={true}
+                onIndexSelected={(index) => setSelectedIndex(index)}
               />
-              <Tab
-                label="Labels"
-                disabled={result.labelAnnotations == null}
-                sx={{
-                  "&:focus": {
-                    outline: "none",
-                  },
-                }}
+            )}
+            {selectedTab === 1 && result.labelAnnotations != null && (
+              <LabelDetectionResultView annotations={result.labelAnnotations} />
+            )}
+            {selectedTab === 2 && result.imagePropertiesAnnotation != null && (
+              <ImagePropertiesResultView
+                annotation={result.imagePropertiesAnnotation}
               />
-              <Tab
-                label="Properties"
-                disabled={result.imagePropertiesAnnotation == null}
-                sx={{
-                  "&:focus": {
-                    outline: "none",
-                  },
-                }}
+            )}
+            {selectedTab === 3 && result.safeSearchAnnotation != null && (
+              <SafeSearchResultView annotation={result.safeSearchAnnotation} />
+            )}
+            {selectedTab === 4 && result.faceAnnotations != null && (
+              <FaceAnnotationsResultView
+                annotations={result.faceAnnotations}
+                onIndexSelected={(index) => setSelectedIndex(index)}
               />
-              <Tab
-                label="Safe Search"
-                disabled={result.safeSearchAnnotation == null}
-                sx={{
-                  "&:focus": {
-                    outline: "none",
-                  },
-                }}
-              />
-              <Tab
-                label="Faces"
-                disabled={result.faceAnnotations == null}
-                sx={{
-                  "&:focus": {
-                    outline: "none",
-                  },
-                }}
-              />
-            </Tabs>
-            <Box padding={2}>
-              <TabPanel value={selectedTab} index={0}>
-                {result.localizedObjectAnnotations != null ? (
-                  <ObjectDetectionResultView
-                    annotations={result.localizedObjectAnnotations}
-                    showTopResult={true}
-                    onIndexSelected={(index) => setSelectedIndex(index)}
-                  />
-                ) : null}
-              </TabPanel>
-              <TabPanel value={selectedTab} index={1}>
-                {result.labelAnnotations != null ? (
-                  <LabelDetectionResultView
-                    annotations={result.labelAnnotations}
-                  />
-                ) : null}
-              </TabPanel>
-              <TabPanel value={selectedTab} index={2}>
-                {result.imagePropertiesAnnotation != null ? (
-                  <ImagePropertiesResultView
-                    annotation={result.imagePropertiesAnnotation}
-                  />
-                ) : null}
-              </TabPanel>
-              <TabPanel value={selectedTab} index={3}>
-                {result.safeSearchAnnotation != null ? (
-                  <SafeSearchResultView
-                    annotation={result.safeSearchAnnotation}
-                  />
-                ) : null}
-              </TabPanel>
-              <TabPanel value={selectedTab} index={4}>
-                {result.faceAnnotations != null ? (
-                  <FaceAnnotationsResultView
-                    annotations={result.faceAnnotations}
-                    onIndexSelected={(index) => setSelectedIndex(index)}
-                  />
-                ) : null}
-              </TabPanel>
-            </Box>
-          </Paper>
-        </Grid>
-      </Grid>
-    </Paper>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
